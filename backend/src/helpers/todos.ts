334b0b4223181e885644d06d5459d6220873333f
duyhuynhdn <duyhuynh.dn@gmail.com>
2022-07-11 06:23:52 +0700

    Save

--- backend/src/helpers/todos.ts
+++ backend/src/helpers/todos.ts
@@ -1,10 +1,18 @@
-// import { TodosAccess } from './todosAcess'
+import { TodoItem } from './../models/TodoItem';
 // import { AttachmentUtils } from './attachmentUtils';
-import { TodoItem } from '../models/TodoItem'
 import { CreateTodoRequest } from '../requests/CreateTodoRequest'
 import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
 import { createLogger } from '../utils/logger'
 import * as uuid from 'uuid'
 import * as createError from 'http-errors'
+import * as AWS from 'aws-sdk';
+import { TodosAccess } from './todosAcess';
+
+
+const todosAccess = new TodosAccess();
+
 
 // TODO: Implement businessLogic
+export const getTodosForUser = async (): Promise<TodoItem[]> => {
+    return todosAccess.getTodosForUser();
+}
--- backend/src/helpers/todosAcess.ts
+++ backend/src/helpers/todosAcess.ts
@@ -6,7 +6,36 @@ import { TodoItem } from '../models/TodoItem'
 import { TodoUpdate } from '../models/TodoUpdate';
 
 const XAWS = AWSXRay.captureAWS(AWS)
-
 const logger = createLogger('TodosAccess')
-
 // TODO: Implement the dataLayer logic
+export class TodosAccess {
+
+    constructor(
+        private readonly docClient: DocumentClient = createDynamoDBClient(),
+        private readonly toDoTable = process.env.TODOS_TABLE) {
+    }
+
+    async getTodosForUser(): Promise<TodoItem[]> {
+        const result = await this.docClient.query({
+            TableName: this.toDoTable,
+            KeyConditionExpression: 'userId = :userId',
+            ExpressionAttributeValues: {
+                ':userId': '1'
+            }
+        }).promise()
+        const items = result.Items
+        return items as TodoItem[];
+    }
+}
+
+function createDynamoDBClient() {
+    if (process.env.IS_OFFLINE) {
+        console.log('Creating a local DynamoDB instance')
+        return new XAWS.DynamoDB.DocumentClient({
+            region: 'localhost',
+            endpoint: 'http://localhost:8000'
+        })
+    }
+
+    return new XAWS.DynamoDB.DocumentClient()
+}
